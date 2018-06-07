3778498ce7fb4568c925e9320ef55ab5d40892ce
Pascal Senn (GIAPSE) <pascal.senn@gia.ch>
2018-06-07 12:05:24 +0200

    feat(Reflow): adds recompose shallow equal

--- src/Reflow.tsx
+++ src/Reflow.tsx
@@ -1,13 +1,13 @@
-import * as React from "react";
-
-interface IReflowProps {
-  children: any;
-  trigger: any;
-}
+import React from "react";
+import { IReflowProps } from "./interfaces/IReflowProps";
+import shallowEqual from "recompose/shallowEqual";
 
 export class Reflow extends React.Component<IReflowProps> {
   public shouldComponentUpdate(nextProps: IReflowProps): boolean {
-    return this.props.trigger !== nextProps.trigger;
+    return (
+      this.props.trigger !== nextProps.trigger ||
+      !shallowEqual(this.props.oneOf, nextProps.oneOf)
+    );
   }
 
   public render(): JSX.Element {
---
+++ src/interfaces/IReflowProps.ts
@@ -0,0 +1,5 @@
+export interface IReflowProps {
+  children?: any;
+  trigger?: any;
+  oneOf?: any[];
+}
